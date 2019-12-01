50085e78bf326c1dfce9b7d3b54c477f3969db5b
Thingo314 <sam.huynh314@gmail.com>
2019-12-01 16:37:56 +1100

    Made edits to solution template
     - Added print statements for answers

--- Day_Template/Day_Num.java
+++ Day_Template/Day_Num.java
@@ -4,5 +4,7 @@ public class Day_Num {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		sc.close();
+		System.out.println("Part 1: ");
+		System.out.println("Part 2: ");
 	}
 }

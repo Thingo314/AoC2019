e34c9821a12a1441076821799f3075d9228e096c
Thingo314 <sam.huynh314@gmail.com>
2019-12-09 20:00:16 +1100

    Solution to Day 9

---
+++ Day_09/Day_09.java
@@ -0,0 +1,136 @@
+import java.util.*;
+
+public class Day_09 {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		String[] memory = sc.nextLine().split(",");
+
+		long[] values = new long[memory.length];
+		for (int i = 0; i < memory.length; i++)
+			values[i] = Long.parseLong(memory[i]);
+
+		IntCode comp = new IntCode(values);
+
+		System.out.println("Part 1: " + comp.runProgram(new long[]{1}));
+		comp.reset();
+		System.out.println("Part 2: " + comp.runProgram(new long[]{2}));
+	}
+}
+
+class IntCode {
+	long[] values = new long[1];
+	long[] copy = new long[1];
+	int pointer = 0;
+	int relativeBase = 0;
+
+	public IntCode(long[] mem) {
+		values = Arrays.copyOf(mem, mem.length * 3);
+		copy = values.clone();
+	}
+
+	public long runProgram(long[] input) {
+		int inIndex = 0;
+		if (pointer != 0)
+			inIndex = 1;
+
+		while (pointer < values.length) {
+			int instruction = (int) values[pointer];
+			int opcode = instruction % 100;
+			instruction /= 100;
+
+			int[] modes = new int[3];
+			for (int i = 0; i < 3; i++) {
+				modes[i] = instruction % 10;
+				instruction /= 10;
+			}
+
+			long[] parameters = new long[3];
+			if (opcode == 3 || opcode == 4 || opcode == 9) {
+				if (modes[0] == 0) {
+					parameters[0] = values[pointer + 1];
+				} else if (modes[0] == 1) {
+					parameters[0] = pointer + 1;
+				} else if (modes[0] == 2) {
+					parameters[0] = values[pointer + 1] + relativeBase;
+				}
+			} else if (opcode != 99) {
+				for (int i = 0; i < 2; i++) {
+					if (modes[i] == 0) {
+						parameters[i] = values[(int)values[pointer + i + 1]];
+					} else if (modes[i] == 1) {
+						parameters[i] = values[pointer + i + 1];
+					} else if (modes[i] == 2) {
+						parameters[i] = values[(int)values[pointer + i + 1] + relativeBase];
+					}
+				}
+				if (modes[2] == 0) {
+					parameters[2] = (int)values[pointer + 3];
+				} else if (modes[2] == 2) {
+					parameters[2] = (int)values[pointer + 3] + relativeBase;
+				}
+			} else {
+				return -1;
+			}
+
+			switch (opcode) {
+				case 1:
+					values[(int)parameters[2]] = parameters[0] + parameters[1];
+					pointer += 4;
+					break;
+				case 2:
+					values[(int)parameters[2]] = parameters[0] * parameters[1];
+					pointer += 4;
+					break;
+				case 3:
+					values[(int)parameters[0]] = input[inIndex];
+					pointer += 2;
+					inIndex++;
+					break;
+				case 4:
+					pointer += 2;
+					return values[(int)parameters[0]];
+				case 5:
+					if (parameters[0] != 0) {
+						pointer = (int)parameters[1];
+					} else {
+						pointer += 3;
+					}
+					break;
+				case 6:
+					if (parameters[0] == 0) {
+						pointer = (int)parameters[1];
+					} else {
+						pointer += 3;
+					}
+					break;
+				case 7:
+					if (parameters[0] < parameters[1]) {
+						values[(int)parameters[2]] = 1;
+					} else {
+						values[(int)parameters[2]] = 0;
+					}
+					pointer += 4;
+					break;
+				case 8:
+					if (parameters[0] == parameters[1]) {
+						values[(int)parameters[2]] = 1;
+					} else {
+						values[(int)parameters[2]] = 0;
+					}
+					pointer += 4;
+					break;
+				case 9:
+					relativeBase += values[(int)parameters[0]];
+					pointer += 2;
+					break;
+			}
+		}
+		return -1;
+	}
+
+	public void reset() {
+		values = copy.clone();
+		pointer = 0;
+		relativeBase = 0;
+	}
+}
